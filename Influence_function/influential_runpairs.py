02f513611c570f06d8cd071b82a7ed42f1181c05
lindsey98 <kelseyliu1998@gmail.com>
2022-01-06 22:03:38 +0800

    minor

--- Influence_function/influential_runpairs.py
+++ Influence_function/influential_runpairs.py
@@ -8,9 +8,9 @@ os.environ['CUDA_VISIBLE_DEVICES'] = "0,1"
 
 if __name__ == '__main__':
 
-    dataset_name = 'cub'
+    dataset_name = 'cars'
     loss_type = 'ProxyNCA_pfix'
-    config_name = 'cub'
+    config_name = 'cars'
     sz_embedding = 512
     seed = 4
     test_crop = False
@@ -44,88 +44,87 @@ if __name__ == '__main__':
 
     '''Step 3: Train the model for every pair'''
     # Run in shell
-    # for kk in tqdm(range(min(len(wrong_indices), 50))):
-    #     wrong_ind = wrong_indices[kk]
-    #     confuse_ind = confuse_indices[kk]
-    #     #  Normal training
-    #     os.system("python train_sample_reweight.py --dataset {} \
-    #                     --loss-type ProxyNCA_pfix_confusion_{}_{}_Allsamples \
-    #                     --helpful {}/Allhelpful_indices_{}_{}.npy \
-    #                     --harmful {}/Allharmful_indices_{}_{}.npy \
-    #                     --model_dir {} \
-    #                     --helpful_weight 2 --harmful_weight 0 \
-    #                     --seed {} --config config/{}_reweight.json".format(IS.dataset_name,
-    #                                                                        wrong_ind, confuse_ind,
-    #                                                                        base_dir, wrong_ind, confuse_ind,
-    #                                                                        base_dir, wrong_ind, confuse_ind,
-    #                                                                        IS.model_dir,
-    #                                                                        IS.seed,
-    #                                                                        IS.dataset_name))
-    #     # reverse training
-    #     os.system("python train_sample_reweight.py --dataset {} \
-    #                     --loss-type ProxyNCA_pfix_confusion_{}_{}_Allsamples \
-    #                     --helpful {}/Allhelpful_indices_{}_{}.npy \
-    #                     --harmful {}/Allharmful_indices_{}_{}.npy \
-    #                     --model_dir {} \
-    #                     --helpful_weight 0 --harmful_weight 2 \
-    #                     --seed {} --config config/{}_reweight.json".format(IS.dataset_name,
-    #                                                                        wrong_ind, confuse_ind,
-    #                                                                        base_dir, wrong_ind, confuse_ind,
-    #                                                                        base_dir, wrong_ind, confuse_ind,
-    #                                                                        IS.model_dir,
-    #                                                                        IS.seed,
-    #                                                                        IS.dataset_name))
-    # exit()
-
-    '''Step 4: Sanity check: Whether the confusion pairs are pulled far apart, Whether the confusion samples is pulled closer to correct neighbor'''
-    result_log_file = 'Confuse_pair_influential_data/{}_pairs.txt'.format(IS.dataset_name)
-    IS.model = IS._load_model()  # reload the original weights
-    new_features = IS.get_features()
-    for kk in range(min(len(wrong_indices), 50)):
+    for kk in tqdm(range(min(len(wrong_indices), 50))):
         wrong_ind = wrong_indices[kk]
         confuse_ind = confuse_indices[kk]
-        # Skip written models
-        if os.path.exists(result_log_file):
-            lines = open(result_log_file).readlines()
-            have_written = False
-            for l in lines:
-                if '{}\t{}'.format(wrong_ind, confuse_ind) in l:
-                    have_written = True
-                    break
-            if have_written:
-                continue
+        #  Normal training
+        os.system("python train_sample_reweight.py --dataset {} \
+                        --loss-type ProxyNCA_pfix_confusion_{}_{}_Allsamples \
+                        --helpful {}/Allhelpful_indices_{}_{}.npy \
+                        --harmful {}/Allharmful_indices_{}_{}.npy \
+                        --model_dir {} \
+                        --helpful_weight 2 --harmful_weight 0 \
+                        --seed {} --config config/{}_reweight.json".format(IS.dataset_name,
+                                                                           wrong_ind, confuse_ind,
+                                                                           base_dir, wrong_ind, confuse_ind,
+                                                                           base_dir, wrong_ind, confuse_ind,
+                                                                           IS.model_dir,
+                                                                           IS.seed,
+                                                                           IS.dataset_name))
+        # reverse training
+        os.system("python train_sample_reweight.py --dataset {} \
+                        --loss-type ProxyNCA_pfix_confusion_{}_{}_Allsamples \
+                        --helpful {}/Allhelpful_indices_{}_{}.npy \
+                        --harmful {}/Allharmful_indices_{}_{}.npy \
+                        --model_dir {} \
+                        --helpful_weight 0 --harmful_weight 2 \
+                        --seed {} --config config/{}_reweight.json".format(IS.dataset_name,
+                                                                           wrong_ind, confuse_ind,
+                                                                           base_dir, wrong_ind, confuse_ind,
+                                                                           base_dir, wrong_ind, confuse_ind,
+                                                                           IS.model_dir,
+                                                                           IS.seed,
+                                                                           IS.dataset_name))
+    exit()
 
-        new_weight_path = 'models/dvi_data_{}_{}_loss{}_{}_{}/ResNet_512_Model/Epoch_{}/{}_{}_trainval_{}_{}.pth'.format(
-                           dataset_name,
-                           seed,
-                           'ProxyNCA_pfix_confusion_{}_{}_Allsamples'.format(
-                           wrong_ind, confuse_ind),
-                           2, 0,
-                           1, dataset_name,
-                           dataset_name,
-                           512, seed) # reload weights as new
-
-        new_reverse_weight_path = 'models/dvi_data_{}_{}_loss{}_{}_{}/ResNet_512_Model/Epoch_{}/{}_{}_trainval_{}_{}.pth'.format(
-                           dataset_name,
-                           seed,
-                           'ProxyNCA_pfix_confusion_{}_{}_Allsamples'.format(
-                           wrong_ind, confuse_ind),
-                           0, 2,
-                           1, dataset_name,
-                           dataset_name,
-                           512, seed) # reload weights as new
-
-        IS.model = IS._load_model()  # reload the original weights
-        inter_dist_orig, _ = grad_confusion_pair(IS.model, new_features, [wrong_ind], [confuse_ind])
-
-        IS.model.load_state_dict(torch.load(new_weight_path))
-        inter_dist_after, _ = grad_confusion_pair(IS.model, new_features, [wrong_ind], [confuse_ind])
-
-        IS.model.load_state_dict(torch.load(new_reverse_weight_path))
-        inter_dist_after_reverse, _ = grad_confusion_pair(IS.model, new_features, [wrong_ind], [confuse_ind])
-
-        # log results
-        with open(result_log_file, 'a+') as f:
-            f.write('{}\t{}\t{:.4f}\t{:.4f}\t{:.4f}\n'.format(wrong_ind, confuse_ind, inter_dist_orig,
-                                                              inter_dist_after, inter_dist_after_reverse))
-        # exit()
+    '''Step 4: Sanity check: Whether the confusion pairs are pulled far apart, Whether the confusion samples is pulled closer to correct neighbor'''
+    # result_log_file = 'Confuse_pair_influential_data/{}_pairs.txt'.format(IS.dataset_name)
+    # IS.model = IS._load_model()  # reload the original weights
+    # new_features = IS.get_features()
+    # for kk in range(min(len(wrong_indices), 50)):
+    #     wrong_ind = wrong_indices[kk]
+    #     confuse_ind = confuse_indices[kk]
+    #     # Skip written models
+    #     if os.path.exists(result_log_file):
+    #         lines = open(result_log_file).readlines()
+    #         have_written = False
+    #         for l in lines:
+    #             if '{}\t{}'.format(wrong_ind, confuse_ind) in l:
+    #                 have_written = True
+    #                 break
+    #         if have_written:
+    #             continue
+    #
+    #     new_weight_path = 'models/dvi_data_{}_{}_loss{}_{}_{}/ResNet_512_Model/Epoch_{}/{}_{}_trainval_{}_{}.pth'.format(
+    #                        dataset_name,
+    #                        seed,
+    #                        'ProxyNCA_pfix_confusion_{}_{}_Allsamples'.format(
+    #                        wrong_ind, confuse_ind),
+    #                        2, 0,
+    #                        1, dataset_name,
+    #                        dataset_name,
+    #                        512, seed) # reload weights as new
+    #
+    #     new_reverse_weight_path = 'models/dvi_data_{}_{}_loss{}_{}_{}/ResNet_512_Model/Epoch_{}/{}_{}_trainval_{}_{}.pth'.format(
+    #                        dataset_name,
+    #                        seed,
+    #                        'ProxyNCA_pfix_confusion_{}_{}_Allsamples'.format(
+    #                        wrong_ind, confuse_ind),
+    #                        0, 2,
+    #                        1, dataset_name,
+    #                        dataset_name,
+    #                        512, seed) # reload weights as new
+    #
+    #     IS.model = IS._load_model()  # reload the original weights
+    #     inter_dist_orig, _ = grad_confusion_pair(IS.model, new_features, [wrong_ind], [confuse_ind])
+    #
+    #     IS.model.load_state_dict(torch.load(new_weight_path))
+    #     inter_dist_after, _ = grad_confusion_pair(IS.model, new_features, [wrong_ind], [confuse_ind])
+    #
+    #     IS.model.load_state_dict(torch.load(new_reverse_weight_path))
+    #     inter_dist_after_reverse, _ = grad_confusion_pair(IS.model, new_features, [wrong_ind], [confuse_ind])
+    #
+    #     # log results
+    #     with open(result_log_file, 'a+') as f:
+    #         f.write('{}\t{}\t{:.4f}\t{:.4f}\t{:.4f}\n'.format(wrong_ind, confuse_ind, inter_dist_orig,
+    #                                                           inter_dist_after, inter_dist_after_reverse))
